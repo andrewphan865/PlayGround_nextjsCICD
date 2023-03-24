9f52465f8392b68332e0c8b9d2791aeef7b5e240
andrewphan865@gmail.com <andrewphan865@gmail.com>
2023-03-24 22:17:00 +1030

    add env file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    APP_NAME: process.env.APP_NAME,
+    SF_URL: process.env.SF_URL,
+    AB_URL: process.env.AB_URL
 }
-
+}
 module.exports = nextConfig
